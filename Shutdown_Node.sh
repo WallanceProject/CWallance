c534ddd583ce8851dc0b7a82e4f187c4bacc3a32
WallanceProject <56075299+WallanceProject@users.noreply.github.com>
2019-12-10 10:42:48 +0100

    Update Shutdown_Node.sh

--- Shutdown_Node.sh
+++ Shutdown_Node.sh
@@ -8,7 +8,7 @@
 # Copy Key to target: ssh-copy-id pi@ipaddr
 
 # Find All Node IPs
-IPs=$(sudo nmap -sn $(ip -o -f inet addr show | awk '{print $4}' | grep '192') | awk '/Nmap scan report for/{printf $5;}/MAC Address:/{print " => "$3;}' | grep "B8:27:EB" | awk '{print $1}')
+IPs=$(sudo nmap -n -sn $(ip -o -f inet addr show | awk '{print $4}' | grep '192') | awk '/Nmap scan report for/{printf $5;}/MAC Address:/{print " => "$3;}' | grep "B8:27:EB" | awk '{print $1}')
 
 for ip in $IPs
 do
